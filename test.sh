5aaa5554f9d8077180417f11d2537260fb857f50
Romain <romain.bourdain93@gmail.com>
2023-12-22 09:30:37 +0100

    script test.sh terminé (problème : pas d'interruption du programme lex

--- test.sh
+++ test.sh
@@ -2,13 +2,52 @@
 
 TEST_FOLDER="test"
 
+title() {
+  echo -e "\033[34m ------------------ $1 ------------------ \033[0m"
+}
 
-input_file=echo "test$1/in.txt"
+run_test() {
+  test_folder=$1
+  input_file="$test_folder/in.txt"
+  output_file="$test_folder/out.txt"
+  expected_output_file="$test_folder/ref.txt"
+  
+  title "Suppression des anciens fichiers"
+  make clean
 
-echo "Test $1"
-echo input_file
+  title "Compilation"
+  make
 
-make clean
-make
+  title "Test : $test_folder"
+  ./bin/cmat.bin < $input_file > $test_folder/out.txt
 
-bin/cmat.bin -o echo "test$1/out.txt" << echo "test$1/in.txt"
+  if diff -q $output_file $expected_output_file > /test/test.out; then
+    echo "Le test a réussi"
+  else
+    echo "Le test a échoué. Différences détéctées :"
+    diff $output_file $expected_output_file
+  fi
+  
+
+}
+
+# Si un numéro de test est fourni en paramètre
+if [ $# -eq 1 ]; then
+  # Vérifier que le numéro de test est valide
+  test_number=$1
+  test_folder="$TEST_FOLDER/$test_number"
+  if [ -d $test_folder ]; then
+    run_test $test_folder
+  else
+    echo "Numéro de test invalide. Utilisation : $0 [numéro de test]"
+    exit 1
+  fi
+
+else
+  # Exécuter tous les tests
+  for test_folder in $TEST_FOLDER/*; do
+    if [ -d "$test_folder" ]; then
+      run_test $test_folder
+    fi
+  done
+fi

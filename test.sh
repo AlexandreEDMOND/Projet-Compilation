1ec92d464e08dbf58d9e9c31ad1e8502cb9bac7a
Romain <romain.bourdain93@gmail.com>
2023-12-22 10:26:14 +0100

    executable de tests

--- test.sh
+++ test.sh
@@ -8,20 +8,15 @@ title() {
 
 run_test() {
   test_folder=$1
-  input_file="$test_folder/in.txt"
-  output_file="$test_folder/out.txt"
-  expected_output_file="$test_folder/ref.txt"
-  
-  title "Suppression des anciens fichiers"
-  make clean
+  test_number=$2
+  input_file="$test_folder/test.in"
+  output_file="$test_folder/test.out"
+  expected_output_file="$test_folder/ref.out"
 
-  title "Compilation"
-  make
-
-  title "Test : $test_folder"
+  title "Test $test_number"
   ./bin/cmat.bin < $input_file > $test_folder/out.txt
 
-  if diff -q $output_file $expected_output_file > /test/test.out; then
+  if diff -q $output_file $expected_output_file; then
     echo "Le test a réussi"
   else
     echo "Le test a échoué. Différences détéctées :"
@@ -31,13 +26,22 @@ run_test() {
 
 }
 
+compil() {
+  title "Suppression des anciens fichiers"
+  make clean
+
+  title "Compilation"
+  make
+}
+
 # Si un numéro de test est fourni en paramètre
 if [ $# -eq 1 ]; then
   # Vérifier que le numéro de test est valide
   test_number=$1
   test_folder="$TEST_FOLDER/$test_number"
   if [ -d $test_folder ]; then
-    run_test $test_folder
+    compil
+    run_test $test_folder $test_number
   else
     echo "Numéro de test invalide. Utilisation : $0 [numéro de test]"
     exit 1
@@ -46,8 +50,10 @@ if [ $# -eq 1 ]; then
 else
   # Exécuter tous les tests
   for test_folder in $TEST_FOLDER/*; do
+    test_number=$(basename $test_folder)
     if [ -d "$test_folder" ]; then
-      run_test $test_folder
+      compil
+      run_test $test_folder $test_number
     fi
   done
 fi

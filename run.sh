ef47b08070b30632447b544b5c24d785e95ba450
Romain <romain.bourdain93@gmail.com>
2023-12-23 18:06:31 +0100

    update de run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -e
+
+handle_error() {
+    echo "\033[31mUne erreur est survenue lors de l'exécution de $1\033[0m"
+    exit 1
+}
+
+trap 'handle_error $BASH_COMMAND' ERR
+
 echo "\033[34m ------------------ Suppression des anciens fichiers ------------------ \033[0m"
 
 make clean
